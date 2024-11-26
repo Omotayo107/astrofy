f72e23efb4391ea6311029d2c611e0adcae5ae6c
Omotayo107 <takinwale@gmail.com>
2024-11-26 13:04:52 +0000

    Update config.ts
    description

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Omotayo  |Digital Marketer';
-export const SITE_DESCRIPTION = ' digital marketer whos a very talented and I have extensive experience in retail and service. I'm known for my friendliness and enthusiasm about my work."
+export const SITE_DESCRIPTION = ' digital marketer whos a very talented and I have extensive experience in retail and service. I'm known for my enthusiasm about my work."
 ';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
