141ab9faf7240362d10133475ddd65b3ac07ba2f
Omotayo107 <takinwale@gmail.com>
2024-11-26 14:28:14 +0000

    Update config.ts
    Meta title

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Omotayo  |Digital Marketer';
-export const SITE_DESCRIPTION = ' digital marketer whos a very talented and I have extensive experience in retail and service. I'm known for my enthusiasm about my work."
+export const SITE_TITLE = 'Omotayo  Akinwale Jose  |Digital Marketer';
+export const SITE_DESCRIPTION = '  digital marketer whos a very talented and I have extensive experience in retail and service. I'm known for my enthusiasm about my work."
 ';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
